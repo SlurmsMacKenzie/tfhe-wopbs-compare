ba1b500fc53ead628173872028636849f5d00561
Moritz Buhr <moritz.buhr@uol.de>
2024-01-25 16:43:52 +0100

    Add non working bivariate native wopbs comparison

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 use tfhe::integer::gen_keys_crt;
 use tfhe::integer::wopbs::*;
 use tfhe::shortint::parameters::parameters_wopbs_message_carry::WOPBS_PARAM_MESSAGE_4_CARRY_4_KS_PBS;
+use tfhe::shortint::parameters::parameters_wopbs::PARAM_4_BITS_5_BLOCKS;
 use tfhe::shortint::parameters::PARAM_MESSAGE_4_CARRY_4_KS_PBS;
 
-fn main() {
+fn main(){
+    native_crt_bivariate_wopbs();
+    crt_wopbs();
+}
+
+fn native_crt_bivariate_wopbs() {
+    let basis: Vec<u64> = vec![13, 14, 15];
+
+    let param = PARAM_4_BITS_5_BLOCKS;
+
+    let (cks, sks) = gen_keys_crt(param, basis.clone());
+    let wopbs_key = WopbsKey::new_wopbs_key_only_for_wopbs(&cks, &sks);
+
+    let msg_space:u64 = basis.iter().copied().product();
+
+    let clear1 = 1899 % msg_space;
+    let clear2 = 22 % msg_space;
+    let mut ct1 = cks.encrypt_native_crt(clear1);
+    let mut ct2 = cks.encrypt_native_crt(clear2);
+    let biv_lut = wopbs_key.generate_lut_bivariate_native_crt(&ct1, |x,y| if x > y {1} else {0});
+    let ct_res = wopbs_key.bivariate_wopbs_native_crt(&mut ct1, &mut ct2, &biv_lut); 
+    let res = cks.decrypt_native_crt(&ct_res);
+    assert_eq!(res, if clear1 > clear2 {1} else {0});
+}
+
+fn crt_wopbs() {
     let basis: Vec<u64> = vec![13, 14, 15];
 
     let msg_space:u64 = basis.iter().copied().product();
